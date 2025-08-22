658256ea12a3d2fdf7dd5c7668b47cc23f33eb84
Orlando J. Vilau <ovilau@evolution.com>
2025-08-22 13:28:24 +0100

    clean blink config file

--- nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -4,16 +4,8 @@ return {
 	"saghen/blink.cmp",
 	-- optional: provides snippets for the snippet source
 	dependencies = { "rafamadriz/friendly-snippets" },
-
 	-- use a release tag to download pre-built binaries
 	version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
 	opts = {
 		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
 		-- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -28,13 +20,9 @@ return {
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
 		keymap = { preset = "default" },
-
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
-
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
 			menu = {
@@ -43,37 +31,14 @@ return {
 					columns = {
 						{ "kind_icon", "label", "label_description", gap = 1, "source_name" },
 					},
-					-- components = {
-					-- 	kind_icon = {
-					-- 		text = function(ctx)
-					-- 			local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-					-- 			return kind_icon
-					-- 		end,
-					-- 		-- (optional) use highlights from mini.icons
-					-- 		highlight = function(ctx)
-					-- 			local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
-					-- 			return hl
-					-- 		end,
-					-- 	},
-					-- },
 				},
 			},
 			documentation = { auto_show = true, window = { border = border } },
 		},
-
 		signature = { enabled = true, window = { border = border } },
-
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
-
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
 	opts_extend = { "sources.default" },
