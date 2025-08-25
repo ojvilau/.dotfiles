b104822bd01bfaed0ddc196582ad8f23c007dd78
Orlando J. Vilau <ovilau@evolution.com>
2025-08-25 09:57:20 +0100

    replace nvim-tree with neo-tree (testing)

--- nvim/.config/nvim/lua/config/plugins/mini.lua
+++ nvim/.config/nvim/lua/config/plugins/mini.lua
@@ -18,13 +18,38 @@ return {
 		end,
 	},
 	{
-		"nvim-tree/nvim-tree.lua",
+		--[[ 		"nvim-tree/nvim-tree.lua",
 		config = function()
 			require("nvim-tree").setup({
 				git = { timeout = 5000 },
 			})
 
 			vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<Cr>", { desc = "Toggle NvimTree", silent = true })
-		end,
+		end, ]]
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		keys = {
+			{ "<Leader>e", "<Cmd>Neotree reveal toggle<CR>" }, -- change or remove this line if relevant.
+		},
+		opts = {
+			sources = { "filesystem" },
+			close_if_last_window = true,
+			filesystem = {
+				hijack_netrw_behavior = "open_current",
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+					leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+				},
+			},
+		},
 	},
 }
